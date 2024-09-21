b2cbe76cf3f7693d007dbd6e8ad6ba815146c20d
Abeen Khanal <abeenkhanal16@gmail.com>
2024-09-21 18:54:53 +0545

    hello

--- src/component/Common/Header/nav.jsx
+++ src/component/Common/Header/nav.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { IoMdMenu } from "react-icons/io";
 
 const Nav = () => {
     return (
-        <nav className='fixed top-0 left-0 w-screen z-50  '>
-            <div className='flex  w-11/12 mx-auto justify-between py-5 text-white cursor-pointer'>
+        <nav className='fixed top-0 left-0 w-screen z-50 bg-black'>
+            <div className='flex w-11/12 mx-auto justify-between py-5 text-white cursor-pointer items-center'>
                 <div>
                     <h1 className='text-2xl font-bold'>MEETME</h1>
                 </div>
-                <div className='flex'>
-                    <ul className='flex gap-10 '>
+                <div className='hidden md:flex'>
+                    <ul className='flex gap-10'>
                         <li>Home</li>
                         <li>About</li>
                         <li>Services</li>
@@ -16,6 +17,9 @@ const Nav = () => {
                         <li>Blog</li>
                         <li>Contact</li>
                     </ul>
+                </div>
+                <div className='md:hidden'>
+                    <button className='text-3xl'><IoMdMenu className='cursor-pointer'/></button> 
                 </div>
             </div>
         </nav>
--- src/component/Home/Homepage.jsx
+++ src/component/Home/Homepage.jsx
@@ -1,19 +1,34 @@
 import React from 'react'
-import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
+import { BsCalendarDate } from "react-icons/bs";
+
 
 const Homepage = () => {
   return (
-    <div className='bg-white items-center justify-center flex flex-col max-h-screen'>
-      <div className='h-[70vh] w-full bg-gradient-to-r from-[#7670FE] to-[#87F0FE]'>
+    <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-blue-500 to-teal-300">
+    <div className="bg-white w-full max-w-5xl rounded-lg shadow-lg p-6 md:p-10 flex flex-col md:flex-row gap-8">
+      {/* image section */}
+      <div className=''> 
+   <img className='rounded-lg w-full max-w-sm' src="./profile.jpg" alt="image not found" />
+      </div>
+      {/* details sections */}
+      <div>
         <div>
-        <div>
-          <div>
-            <img src="" alt="" />
-          </div>
+          <h3 className='uppercase text-gray-500 text-sm'>Hello Everybody, i am</h3>
+          <h1 className='text-3xl lg:text-4xl font-bold mt-1'>ABEEN KHANAL</h1>
+          <h2 className='text-lg lg:text-xl text-gray-600 font-semibold mt-2'>Junior UI/UX Developer</h2>
+          <p className='text-gray-500 mt-4'>You will begin to realise why this exercise is called the Dickens Pattern (with reference to the ghost showing Scrooge some different futures)</p>
         </div>
+        <div className='mt-6 '>
+          <div className='flex items-center text-gray-600 gap-3'>
+          <BsCalendarDate />31st December, 1992
+          </div>
+          <div>
+
+          </div>
         </div>
       </div>
     </div>
+  </div>
   )
   
 }
